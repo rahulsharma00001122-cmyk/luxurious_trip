034a581dd5925f890522187259e9020fefd8951e
rahulsharma00001122-cmyk <rahulsharma00001122@gmail.com>
2025-12-22 16:08:53 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openService(service) {
-  const box = document.getElementById("serviceBox");
-  const title = document.getElementById("serviceTitle");
-
-  title.innerText = "Book " + service;
-  box.style.display = "block";
-
-  console.log(service + " clicked");
-}
